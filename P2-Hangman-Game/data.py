79dd6e53a1dc481f07c55a2c7e9f793c77757f29
Max-TCHANA <mjtchana@gmail.com>
2021-08-06 20:12:28 -0400

    Project 2 Hangman-Game

---
+++ P2-Hangman-Game/data.py
@@ -0,0 +1,40 @@
+"""This file defines some data, indeed, these are variables 
+useful for the Hangman game  """
+
+# Number of allowed attempts per party
+nb_coups = 8
+
+# Name of the file storing scores
+nom_fichier_scores = "scores"
+
+# list of words of the Hangman game
+liste_mots = [
+    "avatar",
+    "baby",
+    "before",
+    "care",
+    "choice",
+    "dance",
+    "elephant",
+    "file",
+    "garage",
+    "integer",
+    "journal",
+    "kiwi",
+    "love",
+    "labor",
+    "mountain",
+    "narrow",
+    "optimal",
+    "parent",
+    "quantity",
+    "rubber",
+    "savage",
+    "tango",
+    "union",
+    "vanilla",
+    "wage",
+    "xeros",
+    "yankee",
+    "Zebra",
+]
